58a12cd6a2baca0620a34377db711b9fb1f0cacc
kingfisher1337 <kingfisher1337@gmail.com>
2015-08-12 14:54:53 +0200

    use second order trotter expansion in tfi_gs_fixpoint.py

--- tfi_gs_fixpoint.py
+++ tfi_gs_fixpoint.py
@@ -62,7 +62,8 @@ def test_fct(a, A):
         
     return test_fct_impl
 
-g1 = gates.exp_sigmax(tau*h)
+g1 = gates.exp_sigmax(0.5*tau*h)
+#g1 = gates.exp_sigmax(tau*h)
 g1 = [(0, g1), (1, g1)]
 g2 = gates.exp_sigmaz_sigmaz(tau)
 g2 = [(0, 0, g2), (0, 1, g2), (1, 0, g2), (1, 1, g2)]
@@ -75,7 +76,7 @@ lut = util.build_lattice_lookup_table([[1,0],[1,0]], [4,4])
 
 logfilename = "imtimeev_E_D={:d}_chi={:d}_h={:e}_tau={:.0e}.dat".format(D, chi, h, tau)
 logfile = open(basepath + logfilename, "a")
-a, env = tebd.itebd(a, lut, g1, g2, "random", err=tebderr, tebd_max_iterations=maxiterations, ctmrg_chi=chi, ctmrg_test_fct=test_fct, verbose=True, logfile=logfile, fast_full_update=fast_full_update)
+a, env = tebd.itebd(a, lut, g1, g2, "random", err=tebderr, tebd_max_iterations=maxiterations, ctmrg_chi=chi, ctmrg_test_fct=test_fct, verbose=True, logfile=logfile, fast_full_update=fast_full_update, apply_g1_twice=True)
 logfile.close()
 
 peps.save(a, lut, basepath + "state_D={:d}_chi={:d}_h={:e}_tau={:.0e}.peps".format(D, chi, h, tau))
