4f75c772316d0a2ec99ed003c0250bb9b42d55e2
kingfisher1337 <kingfisher1337@gmail.com>
2015-08-26 07:49:02 +0200

    corrected file permissions

--- tfi_gs_2d.py
+++ tfi_gs_2d.py
@@ -19,7 +19,7 @@ fast_full_update = "-ffu" in sys.argv
 trotter_second_order = "-trotter2" in sys.argv
 output_to_terminal = "-writehere" in sys.argv
 
-f = open("output/global.log")
+f = open("output/global.log", "w")
 f.write("tfi_gs_2d.py pid={:d}, D={:d}, chi={:d}, h={:f}, tau={:.0e}, iterations={:d}, trotter order {:d}{:s}\n".format(os.getpid(), D, chi, h, tau, maxiterations, 2 if trotter_second_order else 1, ", ffu" if fast_full_update else ""))
 f.close()
 
