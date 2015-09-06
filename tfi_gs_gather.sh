42c87f1e2ee2925e84cf72fa6fdf62d6adf24a98
kingfisher1337 <kingfisher1337@gmail.com>
2015-09-06 16:40:45 +0200

    added file for pm-sweep

--- tfi_gs_gather.sh
+++ tfi_gs_gather.sh
@@ -2,15 +2,25 @@
 
 cd output_tfi
 rm "h_mz_E_D=2_chi=20.dat"
+rm "h_mz_E_D=2_chi=20_pm.dat"
 rm "h_mz_E_D=3_chi=30.dat"
 
-for f in `ls D=2_chi=20_*_itebd.dat`
+for f in `ls D=2_chi=20_*_trotter2_itebd.dat`
 do
     h=`echo $f | cut -f 3 -d "_" | cut -f 2 -d "="`
     mz=`tail -n 1 $f | cut -f 3 -d " "`
     E=`tail -n 1 $f | cut -f 5 -d " "`
     tau=`echo $f | cut -f 4 -d "_" | cut -f 2 -d "="`
     echo $h $mz $E $tau >> "h_mz_E_D=2_chi=20.dat"
+done
+
+for f in `ls D=2_chi=20_*_trotter2_pm_itebd.dat`
+do
+    h=`echo $f | cut -f 3 -d "_" | cut -f 2 -d "="`
+    mz=`tail -n 1 $f | cut -f 3 -d " "`
+    E=`tail -n 1 $f | cut -f 5 -d " "`
+    tau=`echo $f | cut -f 4 -d "_" | cut -f 2 -d "="`
+    echo $h $mz $E $tau >> "h_mz_E_D=2_chi=20_pm.dat"
 done
 
 for f in `ls D=3_chi=30_*_itebd.dat`
