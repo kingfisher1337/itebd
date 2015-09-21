bf813074a137e3c0051c8b29037627354fc08555
kingfisher1337 <kingfisher1337@gmail.com>
2015-09-21 09:57:35 +0200

    added gathering for D=3 pm data

--- tfi_gs_gather.sh
+++ tfi_gs_gather.sh
@@ -4,6 +4,7 @@ cd output_tfi
 rm "h_mz_E_D=2_chi=20.dat"
 rm "h_mz_E_D=2_chi=20_pm.dat"
 rm "h_mz_E_D=3_chi=30.dat"
+rm "h_mz_E_D=3_chi=30_pm.dat"
 
 for f in `ls D=2_chi=20_*_trotter2_itebd.dat`
 do
@@ -30,6 +31,15 @@ do
     E=`tail -n 1 $f | cut -f 5 -d " "`
     tau=`echo $f | cut -f 4 -d "_" | cut -f 2 -d "="`
     echo $h $mz $E $tau >> "h_mz_E_D=3_chi=30.dat"
+done
+
+for f in `ls D=3_chi=30_*_trotter2_pm_itebd.dat`
+do
+    h=`echo $f | cut -f 3 -d "_" | cut -f 2 -d "="`
+    mz=`tail -n 1 $f | cut -f 3 -d " "`
+    E=`tail -n 1 $f | cut -f 5 -d " "`
+    tau=`echo $f | cut -f 4 -d "_" | cut -f 2 -d "="`
+    echo $h $mz $E $tau >> "h_mz_E_D=3_chi=30_pm.dat"
 done
 
 cd ..
