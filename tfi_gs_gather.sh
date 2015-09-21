7f65553243408660dde4d7bb2d5fa6890f6e817d
kingfisher1337 <kingfisher1337@gmail.com>
2015-09-21 10:36:29 +0200

    fixed gathering of normal D=3 results

--- tfi_gs_gather.sh
+++ tfi_gs_gather.sh
@@ -24,7 +24,7 @@ do
     echo $h $mz $E $tau >> "h_mz_E_D=2_chi=20_pm.dat"
 done
 
-for f in `ls D=3_chi=30_*_itebd.dat`
+for f in `ls D=3_chi=30_*_trotter2_itebd.dat`
 do
     h=`echo $f | cut -f 3 -d "_" | cut -f 2 -d "="`
     mz=`tail -n 1 $f | cut -f 3 -d " "`
